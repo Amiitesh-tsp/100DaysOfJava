06948d59a94a7754970ba1583bc7f0d1c10c33d4
Amiitesh TSP <tspamiitesh@gmail.com>
2024-10-13 15:30:53 +0530

    Create Oop.java

---
+++ D17:OOP/Oop.java
@@ -0,0 +1,10 @@
+public class Oop {
+    public static void main(String[] args) {
+
+
+        Car myCar = new Car();
+
+        System.out.println(myCar.make);
+        System.out.println(myCar.model);
+    }
+}
