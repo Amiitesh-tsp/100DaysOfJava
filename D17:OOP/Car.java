6a250f2a6f8b4bc7b1d4b5b2b8b9d1926b2b4595
Amiitesh TSP <tspamiitesh@gmail.com>
2024-10-13 15:32:26 +0530

    Create Car.java

---
+++ D17:OOP/Car.java
@@ -0,0 +1,14 @@
+public class Car {
+    String make = "Hyundai";
+    String model = "Venue";
+    int year = 2021;
+    String color = "blue";
+    double price = 950000.00;
+
+    void drive(){
+        System.out.println("You drive the car");
+    }
+    void brake() {
+        System.out.println("You step on the brake");
+    }
+}
