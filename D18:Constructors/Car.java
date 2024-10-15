ebf9461a70433d8156f243b6edb4c26b60d0f45d
Amiitesh TSP <tspamiitesh@gmail.com>
2024-10-15 10:11:44 +0530

    Create Car.java

---
+++ D18:Constructors/Car.java
@@ -0,0 +1,10 @@
+public class Car {
+    String name;
+    double price;
+    int speed;
+    Car(String name, double price, int speed){
+      this.name = name;
+      this.price = price;
+      this.speed = speed;
+    }
+}
