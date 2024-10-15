3de244890b3d7530b090e1000ed98176668bd639
Amiitesh TSP <tspamiitesh@gmail.com>
2024-10-15 10:12:03 +0530

    Add files via upload

---
+++ D18:Constructors/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Car car1 = new Car("Porsche", 250000, 340);
+        Car car2 = new Car("Hyundai", 15000, 160);
+        System.out.println(car1.name);
+        System.out.println(car2.name);
+
+    }
+}
